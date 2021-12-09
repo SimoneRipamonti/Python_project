9fcb28eb0cd9e9eec194cfda59fb1998c2653181
SimoneRipamonti <simone2.ripamonti@mail.polimi.it>
2021-12-09 16:00:21 +0000

    bc for Darcy

--- class/Flow_class.py
+++ class/Flow_class.py
@@ -24,14 +24,32 @@ class Flow:
         self.data=pp.initialize_data(g, {}, 'flow', parameters)
         self.method=method
         self.Nx=g.num_cells
-    
+        
+    def set_bc(self):
+        tol=1e-4
+        b_faces=self.g.tags["domain_boundary_faces"].nonzero()[0]
+        bc_val=np.zeros(self.g.num_faces)
+        bc_lambda=self.data[pp.PARAMETERS]["flow"]["bc_lambda"]
+        b_face_centers=self.g.face_centers[:,b_faces]
+        
+        for i in range(b_faces.size):
+            bc_val[b_faces[i]]=bc_lambda(b_face_centers[0,i],b_face_centers[1,i],b_face_centers[2,i])
+        
+        bc = pp.BoundaryCondition(self.g, b_faces, ['dir']*b_faces.size)
+        
+        self.data[pp.PARAMETERS]["flow"]["bc"] = bc
+        self.data[pp.PARAMETERS]["flow"]["bc_values"]=bc_val
+        self.data[pp.PARAMETERS]["flow"].pop("bc_lambda")
+        
+        
+        
     def set_source(self):
         f=np.zeros(self.Nx)
         f_lambda=self.data[pp.PARAMETERS]["flow"]["f_lambda"]
         for i in range(self.g.num_cells):
             f[i]=0.01*f_lambda(self.g.cell_centers[0,i],self.g.cell_centers[1,i],self.g.cell_centers[2,i])
         self.data[pp.PARAMETERS]['flow']["source"]=f
-        
+        self.data[pp.PARAMETERS]["flow"].pop("f_lambda")
         
     
     def solve(self):
@@ -47,7 +65,7 @@ class Flow:
         flow_discretization.discretize(self.g, self.data)
         A, b_flow = flow_discretization.assemble_matrix_rhs(self.g, self.data)
         
-        
+        self.data[pp.PARAMETERS]["flow"] 
         if(self.method=="MVEM"):
             rhs_discretization = pp.DualScalarSource("flow")
         else:
